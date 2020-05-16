de9336e1a077d5f71f2972e90ea10a08bedd42cc
Manny Martinez <manel_martinz@hotmail.com>
2020-05-16 16:53:02 -0700

    added-comments-to-code

--- os-dir.py
+++ os-dir.py
@@ -1,9 +1,11 @@
-import os
+import os  #imports the OS
 import sys
 
+#Here is where the user is asked to enter names
 folder_name = input('Enter a folder name: ')
 file_name = input('Enter a file name: ')
 
+#This should provide a message that tells user a file or folder already exists
 folder_path = folder_name
 if not os.path.exists(folder_path):
     try:
@@ -18,10 +20,12 @@ if not os.path.exists(os.path.join(folder_name, folder_path)):
     except FileExistsError:
         print(file_name, 'already exists!')
 
+#Tells user to type info
 name = input('Please enter your name: \n')
 address = input('Please enter your address: \n')
 phone = input('Please eneter your phone number: \n')
 
+#Opens as write and read with proper format.
 with open(file_path, 'w+') as f:
     f.writelines(name + '\n')
     f.writelines(address + '\n')
